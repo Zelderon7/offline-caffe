6890effa3ea29d2a5e3a1143826237ee0c351d9e
Zelderon7 <zelderon038@gmail.com>
2025-10-27 18:06:40 +0200

    Fixed JDBC Connection to Supabase using direct connection

---
+++ src/main/java/controllers/TestController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/film")
+public class TestController {
+
+    @Path("/test")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "test";
+    }
+}
