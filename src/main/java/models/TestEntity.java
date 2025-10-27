6209fb71921000ac1fab9681093a202857a606a8
Zelderon7 <zelderon038@gmail.com>
2025-10-27 14:52:57 +0200

    Clean start

---
+++ src/main/java/models/TestEntity.java
@@ -0,0 +1,12 @@
+package models;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table()
+public class TestEntity extends PanacheEntity {
+    public String name;
+    public String email;
+}
