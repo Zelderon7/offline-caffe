fdb8c049b38bdad876ad5b959ddccd744a7f08d7
Zelderon7 <zelderon038@gmail.com>
2025-11-05 16:53:30 +0200

    Added missing ApplicationScoped annotation to TestRepository

--- src/main/java/repository/TestRepository.java
+++ src/main/java/repository/TestRepository.java
@@ -1,8 +1,10 @@
 package repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import models.Entities.TestEntity;
 
+@ApplicationScoped
 public class TestRepository implements PanacheRepository<TestEntity> {
 
 }
